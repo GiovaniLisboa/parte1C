543435bb326d3ce609de771f632daea4f55cc7eb
Giovani Lisboa <giovanilisboa94@gmail.com>
2017-10-17 00:02:14 -0300

    Add files via upload

---
+++ parte1C.cpp
@@ -0,0 +1,10 @@
+#include "parte1C.h"
+
+parte1C::parte1C()
+{
+}
+
+parte1C::~parte1C()
+{
+}
+
---
+++ parte1C.h
@@ -0,0 +1,12 @@
+#ifndef PARTE1C_H
+#define PARTE1C_H
+
+class parte1C
+{
+public:
+    parte1C();
+    ~parte1C();
+
+};
+
+#endif // PARTE1C_H
