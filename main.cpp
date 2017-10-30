6dfea4a8a1dccdf160991f358cfba75ad5c90a54
Giovani Lisboa <giovanilisboa94gmail.com>
2017-10-30 00:07:09 -0300

    Atualização

--- main.cpp
+++ main.cpp
@@ -7,9 +7,10 @@ int main(int argc, char **argv)
 {
     int selecao; // variável de seleção
     int nota; // seleção 1
-    double nota1, nota2, nota3, media, notafinal; // seleção 2
-    char frequencia; // seleção 2
-    int f, f_1, f_2, cont, sequencia; //seleção 3
+    int valor1, valor2; //seleção 2    
+    double nota1, nota2, nota3, media, notafinal; // seleção 3
+    char frequencia; // seleção 3
+    int f, f_1, f_2, cont, sequencia; //seleção 4
            
     cout << "1. Codigo para indicar o conceito do aluno.\n";
     cout << "2. Tarefa 1 (18.10.2017)\n";
@@ -47,7 +48,30 @@ int main(int argc, char **argv)
         
         case 2:
         {
-            
+                cout << "Insira o primeiro numero: ";
+                cin >> valor1;
+                
+                cout << "Insira o segundo numero: ";
+                cin >> valor2;
+                
+                cout << "\n";
+                
+                if (valor1 > valor2)
+                    {
+                        cout << valor1;
+                        cout << " maior que ";
+                        cout << valor2;
+                    }
+                    else if (valor1 < valor2)
+                         {
+                        cout << valor2;
+                        cout << " maior que ";
+                        cout << valor1;
+                        }
+                        else
+                            cout << "Os valores saaaaao iguais \n";
+                    
+                break;
         }
         
         case 3:
@@ -87,11 +111,18 @@ int main(int argc, char **argv)
         
         case 4:
         {
-            cout << "Insira quantos termos da sequencia de Fibonacci deseja exibir: ";
-            cin >> sequencia;
+            sequencia = 0;
+            
+            while (sequencia < 1 || sequencia > 45)
+            {
+                cout << "Insira quantos termos da sequencia de Fibonacci deseja exibir (1 a 45): ";
+                cin >> sequencia;
+            }
+            
             f_2 = 0;
             f_1 = 0;
             f = 1;
+            
             for (cont = 1; cont <= sequencia; cont++)
             {
                 cout << "f(";
@@ -103,6 +134,7 @@ int main(int argc, char **argv)
                 f_1 = f;
                 f = f_1 + f_2;
             }
+            
             break;
         }
         
